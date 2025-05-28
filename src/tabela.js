14da9eac363724984ac52f42d19307117a4af56b
Jan Bętkowski <112852490+betkowskijan@users.noreply.github.com>
2025-05-28 12:34:52 +0200

    Update tabela.js
    dodanie przykładowych entry w tabeli

--- src/tabela.js
+++ src/tabela.js
@@ -1,7 +1,31 @@
 const tbody = document.getElementById('dane');
 const resetBtn = document.querySelector('.clear');
-
-document.addEventListener('DOMContentLoaded', refreshTable);
+document.addEventListener('DOMContentLoaded', function () {
+  const przyklady = [
+    {
+      name : "Cześć! Chciałabym zapytać o dostępność produktu.",
+      email : "anna.kowalska@email.com",
+      message : "Cześć! Chciałabym zapytać o dostępność produktu."
+    },
+    {
+      name : "Jan Nowak",
+      email : "jan.nowak@email.com",
+      message : "Proszę o więcej informacji na temat oferty."
+    },
+    {
+      name : "Marta Wiśniewska",
+      email : "marta.wisniewska@email.com",
+      message : "Zastanawiam się nad współpracą. Jakie warunki"
+    },
+    {
+      name : "Piotr Zieliński",
+      email : "piotr.zielinski@email.com",
+      message : "Cześć, mam pytanie dotyczące zwrotów."
+    }
+  ]
+  localStorage.setItem('formEntries', JSON.stringify(przyklady));
+  refreshTable()
+});
 
 function refreshTable() {
   tbody.innerHTML = '';
@@ -9,16 +33,22 @@ function refreshTable() {
 }
 
 function loadEntriesFromStorage() {
-  const entries = JSON.parse(localStorage.getItem('formEntries'));
-  entries.forEach((entry) => {
-    const row = document.createElement('tr');
-    row.innerHTML = `
-      <td>${entry.name}</td>
-      <td>${entry.email}</td>
-      <td>${entry.message}</td>
-    `;
-    tbody.appendChild(row);
-  });
+  if (localStorage.getItem('formEntries')) {
+    console.log("JAKAŚ TABELA JEST!")
+    const entries = JSON.parse(localStorage.getItem('formEntries'));
+    entries.forEach((entry) => {
+      const row = document.createElement('tr');
+      row.innerHTML = `
+        <td>${entry.name}</td>
+        <td>${entry.email}</td>
+        <td>${entry.message}</td>
+      `;
+      tbody.appendChild(row);
+    });
+  } else {
+    console.log("PUSTO!")
+    alert("Brak informacji w tabeli! Odśwież strone aby wyświetlić przykładowe informacje!")
+  }
 }
 
 resetBtn.addEventListener('click', function () {
